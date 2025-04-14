ffc1dc0b75c5a265eafe68074a3d35535c29bfcb
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-04-14 16:42:53 +0700

    a tw and font

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Home Page</h1>
+        <p class="text-base font-semibold">Home Page</p>
         <RouterLink to="/about">Go to About</RouterLink>
     </div>
 </template>
